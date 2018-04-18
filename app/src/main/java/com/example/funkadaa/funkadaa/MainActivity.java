1cb48ae0cd1ecaf1d501d166a6fdb844e142f678
AhmadBilalAslam <ahmadbilal07@live.com>
2018-04-17 21:47:30 -0700

    yay

--- app/src/main/java/com/example/funkadaa/funkadaa/MainActivity.java
+++ app/src/main/java/com/example/funkadaa/funkadaa/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.funkadaa.funkadaa;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    Fragment F;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,18 +25,36 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    F= new HomeFragment();
+                    ChangeFrag();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    F= new DashboardFragment();
+                    ChangeFrag();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    F= new NotificationFragment();
+                    ChangeFrag();
                     return true;
-            }
+                    }
+
+
             return false;
         }
     };
 
+
+    void  ChangeFrag()
+{
+
+    FragmentManager manager=getSupportFragmentManager();
+    FragmentTransaction transaction=manager.beginTransaction();
+    transaction.replace(R.id.fragment,F).addToBackStack(null);
+    transaction.commit();
+
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
