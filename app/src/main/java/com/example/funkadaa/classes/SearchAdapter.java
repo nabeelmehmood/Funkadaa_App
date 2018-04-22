a9c2a36dd36d4f551107551e0886dc90ebf78661
AhmadBilalAslam <ahmadbilal07@live.com>
2018-04-22 03:54:11 -0700

    Y

---
+++ app/src/main/java/com/example/funkadaa/classes/SearchAdapter.java
@@ -0,0 +1,61 @@
+package com.example.funkadaa.classes;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.example.funkadaa.funkadaa.R;
+
+public class SearchAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public SearchAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.starrynight, R.drawable.zain12,
+            R.drawable.zain1, R.drawable.zain11,
+            R.drawable.zain2, R.drawable.zain13,
+            R.drawable.zain3, R.drawable.zain14,
+            R.drawable.zain4, R.drawable.zain15,
+            R.drawable.zain5, R.drawable.zain16,
+            R.drawable.zain6, R.drawable.zain17,
+            R.drawable.zain7, R.drawable.zain18,
+            R.drawable.zain8, R.drawable.zain19,
+            R.drawable.zain9, R.drawable.zain10,
+    };
+
+}
--- app/src/main/java/com/example/funkadaa/funkadaa/SearchFragment.java
+++ app/src/main/java/com/example/funkadaa/funkadaa/SearchFragment.java
@@ -2,18 +2,50 @@ package com.example.funkadaa.funkadaa;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import com.example.funkadaa.classes.SearchAdapter;
 
 public class SearchFragment extends Fragment {
+
+    GridView gridview;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
       //  container.removeAllViews();
+
+
+
+
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
-}
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        gridview = (GridView) getView().findViewById(R.id.gridview);
+        Adapter ad =new SearchAdapter(getContext());
+        gridview.setAdapter((ListAdapter) ad);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    }
