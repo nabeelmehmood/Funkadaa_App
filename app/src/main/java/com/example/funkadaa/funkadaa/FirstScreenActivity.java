d008646b7c0fd4321993b11c11cfb6e1ad1acbf3
Nabeel <nabeel_mehmood@hotmail.com>
2018-04-25 05:26:07 +0500

    remove signout

--- app/src/main/java/com/example/funkadaa/funkadaa/FirstScreenActivity.java
+++ app/src/main/java/com/example/funkadaa/funkadaa/FirstScreenActivity.java
@@ -29,8 +29,7 @@ public class FirstScreenActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         mAuth = FirebaseAuth.getInstance();
 
-        mAuth.signOut()
-
+        
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if(currentUser != null)
